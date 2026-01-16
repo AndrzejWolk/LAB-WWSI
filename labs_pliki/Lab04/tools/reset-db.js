5f85d0c7c485402098de012b0776054801398f9a
AndrzejWolk <andzej157@gmail.com>
2026-01-16 09:57:33 +0100

    Delete labs_pliki directory

--- labs_pliki/Lab04/tools/reset-db.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-const DB_FILE = process.env.DB_FILE || 'movies.db';
-if (fs.existsSync(DB_FILE)) fs.rmSync(DB_FILE);
-console.log('Removed', DB_FILE);
--- labs_pliki/Lab06/tools/reset-db.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-const DB_FILE = process.env.DB_FILE || 'notes.db';
-if (fs.existsSync(DB_FILE)) fs.rmSync(DB_FILE);
-console.log('Removed', DB_FILE);
